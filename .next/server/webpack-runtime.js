056a629e5431a8e99eed9d346c9c59f4b3ed4a6f
MatiasFormoso <formosomatiasagustin@gmail.com>
2025-10-09 11:33:38 -0300

    fix(contact): refine copy tone for clarity and consistency

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -194,7 +194,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("791bf31e263fa652")
+/******/ 		__webpack_require__.h = () => ("d8067c1a57cf528f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb6ffbd71b0c4fd8"; }
+/******/ 		__webpack_require__.h = function() { return "c4dc10b687996fdb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
